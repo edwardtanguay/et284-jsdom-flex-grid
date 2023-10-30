bf6ef6cf1aebe3b5da4468dbf33ab8f810903a0e
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-30 22:01:33 +0100

    two styles sheets

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { BoxWrapper } from './components/BoxWrapper'
 import { Ex001 } from './examples/Ex001'
 import { Ex002 } from './examples/Ex002'
 import './style.scss'
+import './main.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<h1 class="text-3xl mb-6">Flex and Grid Practice</h1>
-	${BoxWrapper('Ex001', Ex001)}
 	${BoxWrapper('Ex002', Ex002)}
+	${BoxWrapper('Ex001', Ex001)}
 `
