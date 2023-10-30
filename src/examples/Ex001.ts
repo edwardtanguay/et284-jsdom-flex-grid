1010716fb93f39631a1dc04d53825a8306fb7925
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-30 21:26:04 +0100

    tailwind installed

--- src/examples/Ex001.ts
+++ src/examples/Ex001.ts
@@ -1,5 +1,16 @@
 export const Ex001 = () => {
 	return /*html*/ `
-	the component
+	
+	<div class="ex001">
+		<div class="gec">
+			this is a test
+		</div>
+	</div>
+
+<style>
+.gec {
+	color: blue;
+}
+</style>
 	`
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { Ex001 } from './examples/Ex001'
 import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-	<h1>Flex and Grid Practice</h1>
+	<h1 class="text-3xl mb-4">Flex and Grid Practice</h1>
 	${BoxWrapper(Ex001)}
 `
