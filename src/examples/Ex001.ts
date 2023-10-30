133bd187af5cd1f8d0eb06a9324655be9f19889a
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-30 21:50:57 +0100

    three level responsive

--- src/examples/Ex001.ts
+++ src/examples/Ex001.ts
@@ -2,7 +2,7 @@ export const Ex001 = () => {
 	return /*html*/ `
 	
 	<div class="ex001">
-		<div class="gec">
+		<div class="content">
 			<div class="flow">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis itaque neque modi in eaque iste cumque nobis harum perspiciatis libero molestiae similique ea porro, voluptas pariatur totam aliquam labore aperiam.</div>
 			<div class="flow">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis itaque neque modi in eaque iste cumque nobis harum perspiciatis libero molestiae similique ea porro, voluptas pariatur totam aliquam labore aperiam.</div>
 			<div class="flow">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis itaque neque modi in eaque iste cumque nobis harum perspiciatis libero molestiae similique ea porro, voluptas pariatur totam aliquam labore aperiam.</div>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Ex002 } from './examples/Ex002'
 import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-	<h1 class="text-3xl mb-4">Flex and Grid Practice</h1>
+	<h1 class="text-3xl mb-6">Flex and Grid Practice</h1>
 	${BoxWrapper(Ex001)}
 	${BoxWrapper(Ex002)}
 `
