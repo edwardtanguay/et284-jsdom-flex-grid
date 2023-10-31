e5a50b2eb0801e9daf128fd23a4775583ec4ff61
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-31 13:18:19 +0100

    title span

--- src/components/BoxWrapper.ts
+++ src/components/BoxWrapper.ts
@@ -1,7 +1,7 @@
 export const BoxWrapper = (title: string, func : () => string, description: string = '') => {
 	return /*html*/ `
 	<fieldset class="boxWrapper">
-		<legend>${title}${description ? `<span class="description"> - ${description}</span>` : ''} - <a target="_blank" href="${`https://github.com/edwardtanguay/et284-jsdom-flex-grid/blob/dev/src/examples/${title}.ts`}">html</a> - <a target="_blank" href="https://github.com/edwardtanguay/et284-jsdom-flex-grid/blob/dev/src/main.scss">css</a></legend>
+		<legend><span class="title">${title}<span>${description ? `<span class="description"> - ${description}</span>` : ''} - <a target="_blank" href="${`https://github.com/edwardtanguay/et284-jsdom-flex-grid/blob/dev/src/examples/${title}.ts`}">html</a> - <a target="_blank" href="https://github.com/edwardtanguay/et284-jsdom-flex-grid/blob/dev/src/main.scss">css</a></legend>
 		${func()}
 	</fieldset>
 	`
