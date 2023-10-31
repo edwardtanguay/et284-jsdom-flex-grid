f7034e248ab3e3ffd6462a3fd9aa90da6244153b
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-31 13:40:51 +0100

    ex005

---
+++ src/examples/Ex005.ts
@@ -0,0 +1,12 @@
+export const Ex005 = () => {
+	return /*html*/ `
+	<div class="ex005">
+		<div class="content">
+			<header>This is the header at the top</header>
+			<div class="main">the main area</div>
+			<div class="side">sidebar</div>
+			<footer>This is the footer at the bottom</footer>
+		</div>
+	</div>
+`
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { BoxWrapper } from './components/BoxWrapper'
+import { Ex005 } from './examples/Ex005'
 import { Ex004 } from './examples/Ex004'
 import { Ex003 } from './examples/Ex003'
 import { Ex002 } from './examples/Ex002'
@@ -8,6 +9,7 @@ import './main.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<h1 class="text-3xl mb-6">Flex and Grid Practice</h1>
+	${BoxWrapper('Ex005', Ex005, 'grid-template-areas')}
 	${BoxWrapper('Ex004', Ex004, 'grid-column: 1/3')}
 	${BoxWrapper('Ex003', Ex003, 'minmax(15rem, 100%)')}
 	${BoxWrapper('Ex002', Ex002, 'grid three-width responsive')}
