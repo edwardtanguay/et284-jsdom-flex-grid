fd8559c7a5afb087547b83124c272b271a48be5e
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-02 23:04:30 +0100

    ex009 copied

---
+++ src/examples/Ex009.ts
@@ -0,0 +1,25 @@
+export const Ex009 = () => {
+	return /*html*/ `
+	<div class="ex009">
+		<div class="content">
+			<div class="mb-3 flex gap-3">
+				<span class="pause underline select-none">pause</span>
+				<a class="underline select-none" href="https://www.w3schools.com/cssref/css3_pr_animation.php" target="_blank">see all properties of animation</a>
+			</div>
+			<div class="ball"></div>
+		</div>
+	</div>
+`
+}
+
+setTimeout(() => {
+	const pauseElem = document.querySelector<HTMLSpanElement>('.ex009 .pause');
+	const ballElem = document.querySelector<HTMLDivElement>('.ex009 .ball');
+
+
+	if (pauseElem && ballElem) {
+		pauseElem.addEventListener('click', () => {
+			ballElem.style.animationPlayState = ballElem.style.animationPlayState === 'paused' ? 'running' : 'paused';
+		})
+	}
+})
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { BoxWrapper } from './components/BoxWrapper';
+import { Ex009 } from './examples/Ex009'
 import { Ex008 } from './examples/Ex008'
 import { Ex007 } from './examples/Ex007'
 import { Ex006 } from './examples/Ex006'
@@ -12,6 +13,7 @@ import './exercises.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<h1 class="text-3xl mb-6">Flex, Grid and Animation Practice</h1>
+	${BoxWrapper('Ex009', Ex009, '')}
 	${BoxWrapper('Ex008', Ex008, 'animation - to/from')}
 	${BoxWrapper('Ex007', Ex007, 'CSS variables - animation')}
 	${BoxWrapper('Ex006', Ex006, 'flex order')}
