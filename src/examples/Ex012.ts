68d3760a4f6ff8c6fe2e5f0258d647d46f4c7d46
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-03 01:09:05 +0100

    ex012 finished

---
+++ src/examples/Ex012.ts
@@ -0,0 +1,10 @@
+export const Ex012 = () => {
+	return /*html*/ `
+	<div class="ex012">
+		<div class="content">
+		<p class="row">First Name: <input class="firstName" value="Jonathan"/></p>
+		<p class="row">Last Name: <input class="lastName" value="Jefferson"/></p>
+		</div>
+	</div>
+`
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { BoxWrapper } from './components/BoxWrapper';
+import { Ex012 } from './examples/Ex012'
 import { Ex011 } from './examples/Ex011'
 import { Ex010 } from './examples/Ex010'
 import { Ex009 } from './examples/Ex009'
@@ -15,6 +16,7 @@ import './exercises.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<h1 class="text-3xl mb-6">Flex, Grid and Animation Practice</h1>
+	${BoxWrapper('Ex012', Ex012, 'transition with three properties')}
 	${BoxWrapper('Ex011', Ex011, 'pulsate on hover')}
 	${BoxWrapper('Ex010', Ex010, 'pulsate image with text')}
 	${BoxWrapper('Ex009', Ex009, 'animation percentage')}
